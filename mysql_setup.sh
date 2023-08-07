cc3a859da6791de700ed49ae5e78885a0674ef6d
yoichi-aoyama <aoyama.yoichi@gmail.com>
2023-08-07 23:13:31 +0900

    fix: setup

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -3,11 +3,11 @@
 set -e
 
 # db user
-docker-compose exec mysql mysql -e "CREATE USER 'root'@'%'"
+docker-compose exec mysql mysql -e "CREATE USER IF NOT EXISTS 'root'@'%'"
 docker-compose exec mysql mysql -e "GRANT ALL ON *.* to 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-docker-compose exec mysql mysql -e "CREATE USER 'writer'@'%' IDENTIFIED BY 'writer'"
+docker-compose exec mysql mysql -e "CREATE USER IF NOT EXISTS 'writer'@'%' IDENTIFIED BY 'writer'"
 docker-compose exec mysql mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE ON *.* TO 'writer'@'%';"
-docker-compose exec mysql mysql -e "CREATE USER 'reader'@'%' IDENTIFIED BY 'reader';"
+docker-compose exec mysql mysql -e "CREATE USER IF NOT EXISTS 'reader'@'%' IDENTIFIED BY 'reader';"
 docker-compose exec mysql mysql -e "GRANT SELECT ON *.* TO 'reader'@'%';"
 
 # db
